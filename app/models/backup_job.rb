4aa5b12f35b596ab8757d907a6f550d0955c59b4
Ross Andrews <randrews@HOST-192-168-1-150.private.mylinuxisp.com>
2009-05-04 01:45:21 -0500

    Now writing the RSS content to the file as well

--- app/models/backup_job.rb
+++ app/models/backup_job.rb
@@ -9,10 +9,11 @@ class BackupJob < ActiveRecord::Base
       self.total=rss_pull.num_items ; save
 
       rss_pull.each do |item|
-        logger.info "I should be saving #{item.link} right now"
-
+        handle_item item
         self.finished+=1 ; save
       end
+
+      file_handle.close
 
       self.status='finished' ; save
     rescue
@@ -20,6 +21,28 @@ class BackupJob < ActiveRecord::Base
       self.status='error'
       save
     end
+  end
+
+  private
+
+  def handle_item item
+    separator item.link
+    file_handle.write(open(item.link).read)
+    file_handle.write("\n\n")
+  end
+
+  def separator item_url
+    file_handle.write("#"*80+"\n")
+    file_handle.write("# #{item_url}\n")
+    file_handle.write("#"*80+"\n\n")
+  end
+
+  def file_handle
+    @file_handle ||= File.open(filename,'w')
+  end
+
+  def filename
+    Digest::SHA1.hexdigest(url)+".txt"
   end
 
 end
