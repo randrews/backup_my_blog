91858757a2343e34b15143732f2be1be0b8758a8
Ross Andrews <randrews@HOST-192-168-1-150.private.mylinuxisp.com>
2009-05-04 02:39:39 -0500

    Stupid CSRF protection was causing me problems, like it *ALWAYS* does...

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,13 +3,4 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '42540169454794dacd2cb5438118dd58'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
 end
